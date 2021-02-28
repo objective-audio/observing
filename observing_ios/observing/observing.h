2cceb4e77f8b0cb8929f0765eb339c8ff332813b
Yuki Yasoshima <yukiyasos@gmail.com>
2021-02-28 15:33:12 +0900

    add framework to project

---
+++ observing_ios/observing/observing.h
@@ -0,0 +1,11 @@
+//
+//  observing.h
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for observing.
+FOUNDATION_EXPORT double observingVersionNumber;
+
+//! Project version string for observing.
+FOUNDATION_EXPORT const unsigned char observingVersionString[];
