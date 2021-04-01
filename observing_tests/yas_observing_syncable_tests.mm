0540b24723289d6c5a2545fd03499d086de6045b
Yuki Yasoshima <yukiyasos@gmail.com>
2021-04-01 22:05:15 +0900

    tests

--- observing_tests/yas_observing_syncable_tests.mm
+++ observing_tests/yas_observing_syncable_tests.mm
@@ -16,70 +16,102 @@ using namespace yas::observing;
 @implementation yas_observing_syncable_tests
 
 - (void)test_endable {
-    std::vector<uint32_t> called;
-
-    observing::endable endable{[&called] {
-        return canceller::make_shared(100, [&called](uint32_t const identifier) { called.emplace_back(identifier); });
-    }};
-
-    XCTAssertEqual(called.size(), 0);
-
-    endable.end();
-
-    XCTAssertEqual(called.size(), 1);
-    XCTAssertEqual(called.at(0), 100);
-
-    endable.end();
-
-    XCTAssertEqual(called.size(), 1);
-}
-/*
-- (void)test_syncable_sync {
-    std::size_t handler_called;
+    std::size_t handler_called = 0;
     std::vector<uint32_t> canceller_called;
 
-    observing::syncable syncable{[&handler_called] { handler_called += 1; },
-                                 canceller::make_shared(200, [&canceller_called](uint32_t const identifier) {
-                                     canceller_called.emplace_back(identifier);
-                                 })};
+    observing::endable endable{[&handler_called, &canceller_called] {
+        handler_called++;
+        return canceller::make_shared(
+            100, [&canceller_called](uint32_t const identifier) { canceller_called.emplace_back(identifier); });
+    }};
 
     XCTAssertEqual(handler_called, 0);
     XCTAssertEqual(canceller_called.size(), 0);
 
-    syncable.sync();
+    auto canceller1 = endable.end();
 
     XCTAssertEqual(handler_called, 1);
+    XCTAssertTrue(canceller1);
+
+    canceller1->cancel();
+
+    XCTAssertEqual(canceller_called.size(), 1);
+    XCTAssertEqual(canceller_called.at(0), 100);
+
+    auto canceller2 = endable.end();
+
+    XCTAssertEqual(canceller_called.size(), 1);
+    XCTAssertFalse(canceller2);
+}
+
+- (void)test_endable_empty {
+    observing::endable endable;
+
+    auto canceller = endable.end();
+
+    XCTAssertEqual(canceller, nullptr);
+}
+
+- (void)test_syncable_sync {
+    std::vector<bool> handler_called;
+    std::vector<uint32_t> canceller_called;
+
+    observing::syncable syncable{[&handler_called, &canceller_called](bool const sync) {
+        handler_called.emplace_back(sync);
+        return canceller::make_shared(
+            200, [&canceller_called](uint32_t const identifier) { canceller_called.emplace_back(identifier); });
+    }};
+
+    XCTAssertEqual(handler_called.size(), 0);
+    XCTAssertEqual(canceller_called.size(), 0);
+
+    auto canceller1 = syncable.sync();
+
+    XCTAssertEqual(handler_called.size(), 1);
+    XCTAssertTrue(handler_called.at(0));
+    XCTAssertTrue(canceller1);
+
+    canceller1->cancel();
+
     XCTAssertEqual(canceller_called.size(), 1);
     XCTAssertEqual(canceller_called.at(0), 200);
 
-    syncable.sync();
+    auto canceller2 = syncable.sync();
 
-    XCTAssertEqual(handler_called, 1);
+    XCTAssertEqual(handler_called.size(), 1);
     XCTAssertEqual(canceller_called.size(), 1);
+    XCTAssertFalse(canceller2);
 }
 
 - (void)test_syncable_end {
-    std::size_t handler_called;
+    std::vector<bool> handler_called;
     std::vector<uint32_t> canceller_called;
 
-    observing::syncable syncable{[&handler_called] { handler_called += 1; },
-                                 canceller::make_shared(300, [&canceller_called](uint32_t const identifier) {
-                                     canceller_called.emplace_back(identifier);
-                                 })};
+    observing::syncable syncable{[&handler_called, &canceller_called](bool const sync) {
+        handler_called.emplace_back(sync);
+        return canceller::make_shared(
+            300, [&canceller_called](uint32_t const identifier) { canceller_called.emplace_back(identifier); });
+    }};
 
-    XCTAssertEqual(handler_called, 0);
+    XCTAssertEqual(handler_called.size(), 0);
     XCTAssertEqual(canceller_called.size(), 0);
 
-    syncable.end();
+    auto canceller1 = syncable.end();
 
-    XCTAssertEqual(handler_called, 0);
+    XCTAssertEqual(handler_called.size(), 1);
+    XCTAssertFalse(handler_called.at(0));
+    XCTAssertTrue(canceller1);
+
+    canceller1->cancel();
+
     XCTAssertEqual(canceller_called.size(), 1);
     XCTAssertEqual(canceller_called.at(0), 300);
 
-    syncable.end();
+    auto canceller2 = syncable.end();
 
-    XCTAssertEqual(handler_called, 0);
+    XCTAssertEqual(handler_called.size(), 1);
     XCTAssertEqual(canceller_called.size(), 1);
+    XCTAssertFalse(canceller2);
 }
-*/
+
 @end
