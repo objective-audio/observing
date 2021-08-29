a818f62bc8f5ea6e13f1045fec11f39b2d2c22d3
Yuki Yasoshima <yukiyasos@gmail.com>
2021-08-29 18:57:40 +0900

    fix remover

--- observing/yas_observing_caller_private.h
+++ observing/yas_observing_caller_private.h
@@ -17,8 +17,10 @@ caller<T>::~caller() {
 template <typename T>
 canceller_ptr caller<T>::add(handler_f &&handler) {
     this->_handlers.emplace(this->_next_idx, handler_container{.handler = handler});
-    auto canceller = canceller::make_shared(this->_next_idx,
-                                            [this](uint32_t const idx) { this->_handlers.at(idx).enabled = false; });
+    auto canceller = canceller::make_shared(this->_next_idx, [this](uint32_t const idx) {
+        this->_handlers.at(idx).enabled = false;
+        this->_handlers.erase(idx);
+    });
     this->_cancellers.emplace_back(canceller);
     ++this->_next_idx;
     return canceller;
